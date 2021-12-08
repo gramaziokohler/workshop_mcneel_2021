2a4455f7302494265ceff1980b3d8f37c12b05eb
Romana Rust <rust@arch.ethz.ch>
2021-12-08 18:44:54 +0100

    no robot at that point

--- examples/215_inverse_kinematics.py
+++ examples/215_inverse_kinematics.py
@@ -1,13 +1,16 @@
+import os
 from compas.geometry import Frame
 from compas.robots import Configuration
+from compas.robots import LocalPackageMeshLoader
+from compas.robots import RobotModel
 
-from compas_fab.robots.ur5 import Robot
-from compas_fab.backends.kinematics import AnalyticalInverseKinematics
+from compas_fab.backends.kinematics.solvers import UR5Kinematics
 
-robot = Robot()
-kin = AnalyticalInverseKinematics(solver="ur5")
+models_path = os.path.join(os.path.dirname(__file__), 'models')
+loader = LocalPackageMeshLoader(models_path, 'ur_description')
+model = RobotModel.from_urdf_file(loader.load_urdf('ur5.urdf'))
 
-f = Frame((0.417, 0.191, -0.005), (-0.000, 1.000, 0.000), (1.000, 0.000, 0.000))
+f = Frame((0.417, 0.191, -0.005), (-0.000, 1.000, 0.00), (1.000, 0.000, 0.000))
 
-for jv, jn in kin.inverse_kinematics(robot, f):
-    print(Configuration.from_revolute_values(jv, jn))
+for jv in UR5Kinematics().inverse(f):
+    print(Configuration.from_revolute_values(jv))
--- examples/216_inverse_kinematics_rhino.py
+++ examples/216_inverse_kinematics_rhino.py
@@ -1,17 +1,20 @@
+import os
 from compas.artists import Artist
 from compas.geometry import Frame
-from compas_fab.robots.ur5 import Robot
-from compas_fab.backends.kinematics import AnalyticalInverseKinematics
+from compas.robots import LocalPackageMeshLoader
+from compas.robots import RobotModel
+from compas_fab.backends.kinematics.solvers import UR5Kinematics
 
-robot = Robot()
-kin = AnalyticalInverseKinematics(solver="ur5")
+models_path = os.path.join(os.path.dirname(__file__), 'models')
+loader = LocalPackageMeshLoader(models_path, 'ur_description')
+model = RobotModel.from_urdf_file(loader.load_urdf('ur5.urdf'))
 
-f = Frame((0.417, 0.191, -0.005), (-0.000, 1.000, 0.000), (1.000, 0.000, 0.000))
+f = Frame((0.417, 0.191, -0.005), (-0.000, 1.000, 0.000), (1.000, 0.000, 0.00))
 
-artist = Artist(robot.model, layer='IK')
+artist = Artist(model, layer='IK')
 
-for jv, _ in kin.inverse_kinematics(robot, f):
-    config = robot.model.zero_configuration()
+for jv in UR5Kinematics().inverse(f):
+    config = model.zero_configuration()
     config.joint_values = jv
     artist.update(config)
     artist.draw_visual()
