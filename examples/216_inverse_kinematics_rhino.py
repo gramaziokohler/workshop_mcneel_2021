202f76bf8e050320a5b632e477dcd6815e625454
Romana Rust <rust@arch.ethz.ch>
2021-12-08 18:58:52 +0100

    Update 216_inverse_kinematics_rhino.py

--- examples/216_inverse_kinematics_rhino.py
+++ examples/216_inverse_kinematics_rhino.py
@@ -1,13 +1,12 @@
-import os
 from compas.artists import Artist
 from compas.geometry import Frame
 from compas.robots import LocalPackageMeshLoader
 from compas.robots import RobotModel
 from compas_fab.backends.kinematics.solvers import UR5Kinematics
 
-models_path = os.path.join(os.path.dirname(__file__), 'models')
-loader = LocalPackageMeshLoader(models_path, 'ur_description')
+loader = LocalPackageMeshLoader('models', 'ur_description')
 model = RobotModel.from_urdf_file(loader.load_urdf('ur5.urdf'))
+model.load_geometry(loader)
 
 f = Frame((0.417, 0.191, -0.005), (-0.000, 1.000, 0.000), (1.000, 0.000, 0.00))
 
