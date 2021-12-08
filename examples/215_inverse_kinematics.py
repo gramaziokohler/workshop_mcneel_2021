d38fc08103d388aa4ed75c57ce6556357b384347
Romana Rust <rust@arch.ethz.ch>
2021-12-08 12:22:17 +0100

    updating ik analytical

--- examples/215_inverse_kinematics.py
+++ examples/215_inverse_kinematics.py
@@ -1,20 +1,13 @@
-import os
 from compas.geometry import Frame
-from compas.robots import LocalPackageMeshLoader
-from compas.robots import RobotModel
 from compas.robots import Configuration
 
-from ur_kinematics import inverse_kinematics_ur5
+from compas_fab.robots.ur5 import Robot
+from compas_fab.backends.kinematics import AnalyticalInverseKinematics
 
-models_path = os.path.join(os.path.dirname(__file__), 'models')
-loader = LocalPackageMeshLoader(models_path, 'ur_description')
-model = RobotModel.from_urdf_file(loader.load_urdf('ur5.urdf'))
+robot = Robot()
+kin = AnalyticalInverseKinematics(solver="ur5")
 
 f = Frame((0.417, 0.191, -0.005), (-0.000, 1.000, 0.000), (1.000, 0.000, 0.000))
-f.point /= 0.001
 
-sols = inverse_kinematics_ur5(f)
-
-for joint_values in sols:
-    joint_names = model.get_configurable_joint_names()
-    print(Configuration.from_revolute_values(joint_values, joint_names))
+for jv, jn in kin.inverse_kinematics(robot, f):
+    print(Configuration.from_revolute_values(jv, jn))
