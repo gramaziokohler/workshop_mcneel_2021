b5d975fca89dfaf86b25d45607e5a8f224fb3fa9
Gonzalo Casas <casas@arch.ethz.ch>
2021-12-11 16:56:36 +0100

    Add basic CI

--- examples/014_mesh_plotter.py
+++ examples/014_mesh_plotter.py
@@ -1,10 +1,14 @@
 import compas
 from compas.datastructures import Mesh
-from compas_plotters import MeshPlotter
+from compas_plotters import Plotter
 
 mesh = Mesh.from_obj(compas.get('faces.obj'))
 
-plotter = MeshPlotter(mesh, figsize=(12, 7.5))
-plotter.draw_vertices()
-plotter.draw_faces()
+plotter = Plotter(figsize=(12, 7.5))
+
+meshartist = plotter.add(mesh)
+meshartist.draw_vertices()
+meshartist.draw_faces()
+
+plotter.zoom_extents()
 plotter.show()
