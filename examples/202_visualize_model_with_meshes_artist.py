0b2f2e0c3753d7bc2810434f538eb4df23583e5e
Gonzalo Casas <casas@arch.ethz.ch>
2021-12-12 03:48:26 +0100

    more renames

--- examples/202_visualize_model_with_meshes_artist.py
+++ examples/202_visualize_model_with_meshes_artist.py
@@ -1,5 +1,4 @@
-from compas_rhino.artists import RobotModelArtist
-
+from compas.artists import Artist
 from compas.robots import LocalPackageMeshLoader
 from compas.robots import RobotModel
 
@@ -8,6 +7,6 @@ model = RobotModel.from_urdf_file('models/06_origins_meshes.urdf')
 loader = LocalPackageMeshLoader('models', 'basic')
 model.load_geometry(loader)
 
-artist = RobotModelArtist(model, layer='COMPAS::Robot Viz')
+artist = Artist(model, layer='COMPAS::Robot Viz')
 artist.clear_layer()
 artist.draw_visual()
